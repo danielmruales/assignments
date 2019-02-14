b8133085916d767bcdaade218ac6985113ffa75b
Daniel Ruales <danielmruales@gmail.com>
2019-02-14 09:15:16 -0700

    update for 2-14-19

--- projects/axiostodo/app.js
+++ projects/axiostodo/app.js
@@ -1,21 +1,34 @@
-axios.get("https://api.vschool.io/emily/todo").then(function(response){
-    var arr = response.data
-    for(var i = 0; i < arr.length; i++){
-
-        var todoContainer = document.createElement('div')
-        var input = document.createElement("input")
-        var title = document.createElement("h3")
-        input.type = "checkbox"
-
-        input.addEventListener("click", function(e) {
+axios.get("https://api.vschool.io/daniel/todo").then(function(response){
+var arr = response.data
+for(var i = 0; i < arr.length; i++){
+    
+    var todoContainer = document.createElement("div")
+    var input = document.createElement("input")
+    var title = document.createElement("h3")
+    input.type = "checkbox"
+    
+    input.addEventListener("click", function(e) {
             e.target.parentNode.childNodes[1].style.textDecoration = "line-through";
         }, true)
         
 
-        title.textContent = arr[i].title
+        if(arr[i].completed){
+            todoContainer.classList.toggle("strikened");
+            input.checked = true;
+        }
 
+        title.textContent = arr[i].title
+      
         todoContainer.appendChild(input)
         todoContainer.appendChild(title)
-        document.getElementById("name").appendChild(todoContainer)
+        document.getElementById("divContainer").appendChild(todoContainer)
     }   
 })
+
+// function renderChecked(e){
+// e.target.parentNode.classList.toggle("nostrike")
+// }
+
+input.addEventListener("click", function(e){
+    e.divContainer.style.textDecoration = "none";
+});
